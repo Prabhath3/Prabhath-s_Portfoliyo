29e9d82268366e8ed252926fb255f36222ced94c
Prabhath3 <prabhathnishantha130@gmail.com>
2026-05-27 15:09:00 +0530

    dynamic project showing from GitHub process and html was done

--- portfoliyo/src/components/Project.tsx
+++ portfoliyo/src/components/Project.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
 
+type Repo = {
+    id: number;
+    name: string;
+    description: string;
+    html_url: string;
+    stargazers_count: number;
+}
+
+
 function Projects(){
-    const [projects, setProjects] = useState([]);
+    const [projects, setProjects] = useState<Repo[]>([]);
 
     useEffect(() => {
         async function fetchProject() {
             const response = await fetch (
-                "https://github.com/Prabhath3?tab=repositories"
+                "https://api.github.com/users/Prabhath3/repos"
             );
 
             const data = await response.json();
@@ -19,14 +28,32 @@ function Projects(){
     }, []);
 
     return(
-        <section id="Projects">
-            <div>
-                <p>
+        <section id="Projects" className="bg-zinc-950 text-white px-8 md:px-16 py-24">
+            <div className="mb-16">
+                <p className="text-purple-400 text-lg mb-4">
                     Projects
                 </p>
-                <h2>
+                <h2 className="text-4xl md:texxt-5xl font-bold">
                     My Github Projects
                 </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    {projects.map((repo) => (
+                        <div key={repo.id}>
+                            <h3>
+                                {repo.name}
+                            </h3>
+                            <p>
+                                {repo.description || "No description available"}
+                            </p>
+                            <p>
+                                ⭐ Stars: {repo.stargazers_count}
+                            </p>
+                            <a href={repo.html_url} target="_blank">
+                                View Project →
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
