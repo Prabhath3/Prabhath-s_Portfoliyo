07dd2640c9a245516a2379b3e22ae08a8b650e12
Prabhath3 <prabhathnishantha130@gmail.com>
2026-05-27 14:19:38 +0530

    projects section added to App.tsz

--- portfoliyo/src/App.tsx
+++ portfoliyo/src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Projects from "./components/Project";
+
 
 function App() {
     return (
@@ -10,6 +12,7 @@ function App() {
             <Hero name="Prabhath Nishantha" />
             <About />
             <Skill />
+            <Projects/>
         </div>
     )
 }
